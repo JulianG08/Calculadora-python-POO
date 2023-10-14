2c8cf6983129160e281ce64baa23206497c7aa55
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-14 17:45:13 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from operaciones import *
-from convertir__Numero import *
-
-if __name__ == '__main__':
-    operacion1 = Operacion("", "", "")
-    num01 = int(input("Digite un numero: ")[0:10])
-    #num02 = int(input("Digite otro numero: ")[0:10])
-    print(operacion1.seno(num01))
-    print(numeros_a_palabras(operacion1.seno(num01)))
